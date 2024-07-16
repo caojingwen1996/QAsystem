131e43c090740266b81c4520ee581421e5d685d1
caojingwen.4dbim <2802430545@qq.com>
2024-07-16 10:20:02 +0800

    demo done

--- txt2vec.py
+++ txt2vec.py
@@ -1,14 +1,19 @@
 from langchain_community.document_loaders import UnstructuredFileLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
+# from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from text2vec import SentenceModel
 from transformers import AutoModel
 import os
 import json
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import PromptTemplate
+from langchain_community.llms.tongyi import Tongyi
 
-# os.environ["QIANFAN_AK"] = "v2SKnMKK94eO3IiFjEuC6jio"
-# os.environ["QIANFAN_SK"] = "BAlrTZAWFqHZFeTGrqHANLziKvUFIka0"
+os.environ["DASHSCOPE_API_KEY"] = "sk-190e2fb00a9b4233b1644ebdd080a390" #my
+
 # model_path='/opt/codes/Model/text2vec-base-chinese'
 model_path='../Model/text2vec-base-chinese'
 filename="算力行业周刊.pdf"
@@ -64,6 +69,44 @@ db = FAISS.from_documents(splitted_documents, embeddings)
 save_path=os.path.join("Data","faiss_index_text2vec/doc")
 db.save_local(save_path)
 print("向量索引已保存在：", save_path)
+
+
+# test query
+print("开始query")
+db = FAISS.load_local(save_path, embeddings,allow_dangerous_deserialization=True)
+retriever = db.as_retriever()
+
+template = """Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Use three sentences maximum and keep the answer as concise as possible.
+
+{context}
+
+Question: {question}
+
+Helpful Answer:"""
+custom_rag_prompt = PromptTemplate.from_template(template)
+
+llm = Tongyi(model_name="qwen1.5-72b-chat")
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+def chat_loop(user_input):
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | custom_rag_prompt
+        | llm
+        | StrOutputParser()
+    )
+    return rag_chain.invoke(user_input)
+
+
+user_input = "介绍一下算力情况？"
+while True:
+    response = chat_loop(user_input)
+    print("AI助手：", response)
+    user_input = input("你：")
+
 
 
 # if __name__ == '__main__':
